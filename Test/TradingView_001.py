3591e1c330ede9220f9151c95aef53d5ea9046c9
AlexLan73 <diving_73@mail.ru>
2023-04-13 21:08:04 +0300

    xxxxxxxxxxxx

--- Test/TradingView_001.py
+++ Test/TradingView_001.py
@@ -1,13 +1,13 @@
 import tradingview_ta
 from tradingview_ta import TA_Handler, Interval, Exchange
 
-# tesla = TA_Handler(
-#     symbol="TSLA",
-#     screener="america",
-#     exchange="NASDAQ",
-#     interval=Interval.INTERVAL_1_DAY
-# )
-# print(tesla.get_analysis().summary)
+tesla = TA_Handler(
+    symbol="TSLA",
+    screener="america",
+    exchange="NASDAQ",
+    interval=Interval.INTERVAL_1_DAY
+)
+print(tesla.get_analysis().summary)
 
 print(tradingview_ta.__version__)
 
